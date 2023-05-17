53bed51d9d1461dea7374445e2a3cb7228b18721
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-05-17 11:15:57 -0400

    Gin Initialization Process
    TODO: Add Gin Configuration into Viper
    Add Redis Helper
    Add Jwt Helper

--- Gin/init.go
+++ Gin/init.go
@@ -1,1 +1,40 @@
 package Gin
+
+import (
+	"context"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+)
+
+var r *gin.Engine
+
+func InitializeGinFramework(lc fx.Lifecycle) *gin.Engine {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			r = gin.Default()
+			r.Use(cors.New(cors.Config{
+				AllowAllOrigins:  true,
+				AllowMethods:     []string{"GET", "POST", "OPTIONS", "DELETE", "PUT", "UPDATE"},
+				AllowHeaders:     []string{"Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"},
+				ExposeHeaders:    []string{"Content-Length"},
+				AllowCredentials: true,
+			}))
+			// Middlewares Configuration
+
+			// Router Groups configuration
+
+			// API Configurations
+
+			// Run program
+			go func() {
+				err := r.Run("0.0.0.0:8080")
+				if err != nil {
+					panic("Gin Error")
+				}
+			}()
+			return nil
+		},
+	})
+	return r
+}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"github.com/gin-gonic/gin"
 	"github.com/nitishm/go-rejson/v4"
 	"go.uber.org/fx"
 	"memesa_go_backend/Config"
+	"memesa_go_backend/Gin"
 	"memesa_go_backend/MySQL"
 	"memesa_go_backend/Redis"
 )
@@ -15,10 +17,12 @@ func main() {
 			Config.InitializeViperConfig,
 			MySQL.InitializeMySQLDb,
 			Redis.InitializeRedisDb,
+			Gin.InitializeGinFramework,
 		),
 		// Invokes are here
 		fx.Invoke(func(cfgCluster *Config.ConfigCluster) {}),
 		fx.Invoke(func(SQLDB *sql.DB) {}),
 		fx.Invoke(func(RedisDB *rejson.Handler) {}),
+		fx.Invoke(func(GinFramework *gin.Engine) {}),
 	).Run()
 }
