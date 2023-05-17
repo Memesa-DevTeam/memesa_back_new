957169b7c1cbe040ddadd8eb37928c8411c41f8b
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-05-17 10:37:48 -0400

    MySQL Initialization Process

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"database/sql"
+	"go.uber.org/fx"
+	"memesa_go_backend/Config"
+	"memesa_go_backend/MySQL"
+)
+
+func main() {
+	fx.New(
+		fx.Provide(
+			Config.InitializeViperConfig,
+			MySQL.InitializeMySQLDb,
+		),
+		// Invokes are here
+		fx.Invoke(func(cfgCluster *Config.ConfigCluster) {}),
+		fx.Invoke(func(SQLDB *sql.DB) {}),
+	).Run()
+}
